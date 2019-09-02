243631f1156f68a106a1069d7f3ba1f82bf615ff
Andrei <ankorotkov66@gmail.com>
2019-09-02 12:41:51 +0400

    Initial commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,40 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @BeforeClass
+    public void setup()  {
+        RestAssured.baseURI = "http://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void webStatusTest() {
+        Response response = RestAssured.when()
+                .get("/users")
+                .andReturn();
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+
+    @Test
+    public void webHeaderTest () {
+        Response response = RestAssured.when()
+                .get("/users")
+                .andReturn();
+        String responseHeaderContentType = response.getHeader("Content-type");
+        Assert.assertNotNull(responseHeaderContentType);
+        Assert.assertEquals(responseHeaderContentType, "application/json; charset=utf-8");
+    }
+
+    @Test
+    public void webBodyTest () {
+        Response response = RestAssured.when()
+                .get("/users")
+                .andReturn();
+        User [] users = response.getBody().as(User[].class);
+        Assert.assertEquals(users.length, 10);
+    }
+}
